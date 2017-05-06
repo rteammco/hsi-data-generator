86cef11a9e8f17176a43bd4a3b71dbb9b6db112c
Richard Teammco <teammcr192@gmail.com>
2017-05-06 12:54:09 -0500

    Added navigation tabs and a stylesheet for Qt.

--- src/gui/main_window.cpp
+++ src/gui/main_window.cpp
@@ -2,19 +2,32 @@
 
 #include <iostream>
 
+#include "QAction.h"
 #include "QMenuBar.h"
 #include "QToolBar.h"
+#include "QWidget.h"
+#include "QTabWidget.h"
 
 MainWindow::MainWindow() {
-  QMenu* file_menu = menuBar()->addMenu(tr("&File"));
-  QToolBar* file_tool_bar = addToolBar(tr("File"));
+//  QMenu* file_menu = menuBar()->addMenu(tr("&File"));
+//  QToolBar* file_tool_bar = addToolBar(tr("File"));
+//
+//  QAction* new_action = new QAction(tr("&New"), this);
+//  new_action->setShortcuts(QKeySequence::New);
+//  new_action->setStatusTip(tr("Create a new file"));
+//  connect(new_action, &QAction::triggered, this, &MainWindow::NewFile);
+//  file_menu->addAction(new_action);
+//  file_tool_bar->addAction(new_action);
 
-  QAction* new_action = new QAction(tr("&New"), this);
-  new_action->setShortcuts(QKeySequence::New);
-  new_action->setStatusTip(tr("Create a new file"));
-  connect(new_action, &QAction::triggered, this, &MainWindow::NewFile);
-  file_menu->addAction(new_action);
-  file_tool_bar->addAction(new_action);
+  // Set the tabs:
+  QWidget* central_widget = new QWidget(this);
+  QTabWidget* tabs = new QTabWidget();
+  tabs->setParent(this);
+//  tabs->setFixedSize(330, 220);
+  tabs->addTab(new QWidget(), "Class Spectra");
+  tabs->addTab(new QWidget(), "Image Map");
+  tabs->addTab(new QWidget(), "Add Noise");
+  setCentralWidget(central_widget);
 }
 
 void MainWindow::NewFile() {
--- src/hsi_data_generator.cpp
+++ src/hsi_data_generator.cpp
@@ -1,12 +1,23 @@
-#include <iostream>
+#include <string>
 
 #include "gui/main_window.h"
 
 #include "QApplication.h"
+#include "QFile.h"
+#include "QString.h"
+
+static const std::string kQtStyleSheetPath = "../src/gui/qt_style.qss";
 
 int main(int argc, char** argv) {
-  std::cout << "Hello, world!" << std::endl;
   QApplication app(argc, argv);
+
+  // Load the stylesheet:
+  QFile stylesheet_file(kQtStyleSheetPath.c_str());
+  stylesheet_file.open(QFile::ReadOnly);
+  QString stylesheet_string = QLatin1String(stylesheet_file.readAll());
+  app.setStyleSheet(stylesheet_string);
+
+  // Create the main window:
   MainWindow main_window;
   main_window.show();
   return app.exec();
