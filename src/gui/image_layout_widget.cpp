09cd14e7386ce0ffb391b63b689e50db3e39cc36
Richard Teammco <teammcr192@gmail.com>
2017-06-13 12:35:47 -0500

    Fixed slow rendering of layout view issue.
    Re-implemented the ImageLayoutWidget's paintEvent method to draw more efficiently. It should no longer lag when rendering the view.

--- src/gui/image_layout_widget.cpp
+++ src/gui/image_layout_widget.cpp
@@ -42,28 +42,13 @@ void ImageLayoutWidget::Render(const std::vector<QColor>& class_colors) {
 }
 
 void ImageLayoutWidget::paintEvent(QPaintEvent* event) {
-  QPainter painter(this);
-  painter.setRenderHint(QPainter::Antialiasing, false);
-  QPen point_pen;
-  point_pen.setCapStyle(Qt::RoundCap);
-  point_pen.setWidth(1);
-  painter.setPen(point_pen);
-
-  const int canvas_width = width();
-  const int canvas_height = height();
-  const double row_scale =
-      static_cast<double>(image_layout_->GetHeight()) /
-      static_cast<double>(canvas_height);
-  const double col_scale =
-      static_cast<double>(image_layout_->GetWidth()) /
-      static_cast<double>(canvas_width);
+  const int layout_width = image_layout_->GetWidth();
+  const int layout_height = image_layout_->GetHeight();
   const std::vector<int>& image_class_map = image_layout_->GetClassMap();
-  for (int y = 0; y < canvas_height; ++y) {
-    for (int x = 0; x < canvas_width; ++x) {
-      // Back-project to sample the correct color code from the image class map.
-      const int row_index = static_cast<int>(row_scale * y);
-      const int col_index = static_cast<int>(col_scale * x);
-      const int pixel_index = image_layout_->GetMapIndex(col_index, row_index);
+  QImage image(layout_width, layout_height, QImage::Format_RGB32);
+  for (int x = 0; x < layout_width; ++x) {
+    for (int y = 0; y < layout_height; ++y) {
+      const int pixel_index = image_layout_->GetMapIndex(x, y);
       const int class_index = image_class_map.at(pixel_index);
       QColor color;
       if (class_index < image_class_colors_.size()) {
@@ -74,11 +59,11 @@ void ImageLayoutWidget::paintEvent(QPaintEvent* event) {
                    << " but class index is " << class_index;
         color = Qt::black;
       }
-      point_pen.setColor(color);
-      painter.setPen(point_pen);
-      painter.drawPoint(x, y);
+      image.setPixelColor(x, y, color);
     }
   }
+  QPainter painter(this);
+  painter.drawImage(0, 0, image.scaled(width(), height()));
 }
 
 }  // namespace hsi_data_generator
