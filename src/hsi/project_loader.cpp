0ae9307d8a2bab70bf6fa19b7743c68f81cdfaa1
Richard Teammco <teammcr192@gmail.com>
2017-07-06 14:43:24 -0400

    Project file now saved as XML.
    This allows easier parsing using Qt's built-in tools.

--- src/hsi/project_loader.cpp
+++ src/hsi/project_loader.cpp
@@ -1,82 +1,93 @@
 #include "hsi/project_loader.h"
 
+#include <QFile>
+#include <QIODevice>
 #include <QString>
-
-#include <fstream>
-#include <vector>
+#include <QXmlStreamWriter>
 
 #include "hsi/spectrum.h"
 
 namespace hsi_data_generator {
 namespace {
 
+// Save file constants:
 static const QString kProjectFileHeader =
-    "Project File Exported by HSIDataGenerator.";
+    "Project File Exported by HSIDataGenerator";
 
+static const QString kNumBandsTag = "num_bands";
+static const QString kSpectrumTag = "spectrum";
+static const QString kSpectrumNameTag = "name";
+static const QString kSpectrumColorTag = "color";
+static const QString kSpectrumPeaksTag = "peaks";
+static const QString kPeakTag = "peak";
+static const QString kPeakPositionTag = "position";
+static const QString kPeakAmplitudeTag = "amplitude";
+static const QString kPeakWidthTag = "width";
+
+// Error messages:
 static const QString kSubstitutePlaceholder = "%";
 
 static const QString kGenericErrorMessage =
     "File read/write error.";
-static const QString kFileNotOpenErrorMessage =
+static const QString kFileNotOpenWriteErrorMessage =
     "Could not open file \"" + kSubstitutePlaceholder + "\" for writing.";
+static const QString kFileNotOpenReadErrorMessage =
+    "Could not open file \"" + kSubstitutePlaceholder + "\" for reading.";
 
 }  // namespace
 
 bool ProjectLoader::SaveProjectToFile(const QString& file_name) const {
-  std::ofstream project_file(file_name.toStdString());
-  if (!project_file.is_open()) {
-    error_message_ = kFileNotOpenErrorMessage;
+  QFile project_file(file_name);
+  if (!project_file.open(QIODevice::WriteOnly)) {
+    error_message_ = kFileNotOpenWriteErrorMessage;
     error_message_.replace(kSubstitutePlaceholder, file_name);
     return false;
   }
-  project_file << kProjectFileHeader.toStdString() << "\n\n";
-  project_file << "num_bands = " << num_bands_ << "\n";
-  // Save the spectra information in the following format:
-  //   spectrum = {
-  //     name = Name of Spectrum
-  //     color = #12345f
-  //     peaks = {
-  //       position, amplitude, width
-  //       0.32 0.15 0.58
-  //     }
-  //   }
-  //   spectrum = {
-  //     ...
-  //   }
+  QXmlStreamWriter xml_writer(&project_file);
+  xml_writer.setAutoFormatting(true);
+  xml_writer.writeStartDocument();
+  xml_writer.writeComment(kProjectFileHeader);
+
+  xml_writer.writeTextElement(kNumBandsTag, QString::number(num_bands_));
+
   for (int i = 0; i < spectra_->size(); ++i) {
     const std::shared_ptr<Spectrum> spectrum = spectra_->at(i);
-    const QString spectrum_name = spectrum->GetName();
-    const QString spectrum_color = spectrum->GetColor().name();
-    project_file << "spectrum = {\n";
-    project_file << "  name = " << spectrum_name.toStdString() << "\n";
-    project_file << "  color = " << spectrum_color.toStdString() << "\n";
-    project_file << "  peaks = {\n";
+    xml_writer.writeStartElement(kSpectrumTag);
+    xml_writer.writeTextElement(kSpectrumNameTag, spectrum->GetName());
+    xml_writer.writeTextElement(kSpectrumColorTag, spectrum->GetColor().name());
+    xml_writer.writeStartElement(kSpectrumPeaksTag);
     for (const PeakDistribution& peak : spectrum->GetPeaks()) {
-      project_file << "    " << peak.position << " " << peak.amplitude << " "
-                   << peak.width << "\n";
+      xml_writer.writeStartElement(kPeakTag);
+      xml_writer.writeTextElement(
+          kPeakPositionTag, QString::number(peak.position));
+      xml_writer.writeTextElement(
+          kPeakAmplitudeTag, QString::number(peak.amplitude));
+      xml_writer.writeTextElement(kPeakWidthTag, QString::number(peak.width));
+      xml_writer.writeEndElement();
     }
-    project_file << "  }\n";
-    project_file << "}\n";
+    xml_writer.writeEndElement();
+    xml_writer.writeEndElement();
   }
-  // TODO: Save the layout.
-  // Save the layout information in the following format:
-  // Layout:
-  // layout_width = 100
-  // layout_height = 100
-  // layout {
-  //   row 1
-  //   0 0 0 0 0 1 1 1 2 2 2 1 1 1 0 0 1 ... 0 1 0
-  //   1 2 2 2 2 2 1 1 2 3 3 4 5 5 5 5 ....
-  //   ...
-  //   row 100
-  // }
+
+//  // TODO: Save the layout.
+//  // Save the layout information in the following format:
+//  // Layout:
+//  // layout_width = 100
+//  // layout_height = 100
+//  // layout {
+//  //   row 1
+//  //   0 0 0 0 0 1 1 1 2 2 2 1 1 1 0 0 1 ... 0 1 0
+//  //   1 2 2 2 2 2 1 1 2 3 3 4 5 5 5 5 ....
+//  //   ...
+//  //   row 100
+//  // }
+
   project_file.close();
   return true;
 }
 
 bool ProjectLoader::LoadProjectFromFile(const QString& file_name) {
-  // TODO: Implement;
-  return false;
+  return true;
 }
 
 QString ProjectLoader::GetErrorMessage() const {
